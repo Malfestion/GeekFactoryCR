3fe6424a01bf7906c81f2742a2f890ed8695f28d
Alejandro Duarte <>
2023-03-02 08:05:56 -0600

    Commit inicial

---
+++ src/main/java/com/geekfactory/GeekfactorycrApplication.java
@@ -0,0 +1,13 @@
+package com.geekfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekfactorycrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekfactorycrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekfactory/GeekfactorycrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekfactorycrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
