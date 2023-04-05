070277da030c2b3341007ae048f0bdf7eab7e3c3
Emily <emily.rp@hotmail.com>
2023-04-04 22:04:04 -0600

    Promociones
    1. Se corrigió la imagen del banner haciendola mas pequeña verticalmente y se cambió el texto a nuestro catálogo
    2.se agregó la ímagen de las promociones
    3. todas las imagenes se cambiaron a unas en mejor definición ya que las que estaban estaban pixeleadas, en especial la imagen en donde muestra el reloj, carrito ect...
    4. Se agrego la imagen de las promociones en el apartado de promociones y de igualmanera articulos recomendados para la promoción
    5. se hizo un css aparte para los estilos de las promociones

--- CreaGeekShopCR.sql
+++ CreaGeekShopCR.sql
@@ -1,6 +1,6 @@
 /*Las líneas 2 y 3 están en comentarios, para usarse así la primera vez... luego de eso se quitan los comentarios para que todo funcione bien, en las siguientes ejecuciones */
-drop schema geekfactory;
-drop user administrador;
+/*drop schema geekfactory;
+drop user administrador;*/
 
 /*Se crea la base de datos */
 CREATE SCHEMA geekfactory ;
